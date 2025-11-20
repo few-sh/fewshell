32f197c11df8a299a9a1e3b7961c643d4e1bc90a
Ivgeni Segal <ivgeni.segal@gmail.com>
2025-11-20 12:36:56 -0800

    Remove dead code

--- decamp-app/lib/components/highlight_overlay.dart
+++ decamp-app/lib/components/highlight_overlay.dart
@@ -1,32 +1,5 @@
 import 'package:flutter/material.dart';
 
-/// Widget that wraps content and highlights specific text ranges
-/// Works by overlaying semi-transparent colored boxes at calculated positions
-class HighlightOverlay extends StatelessWidget {
-  final Widget child;
-  final String text;
-  final List<HighlightInfo> highlights;
-
-  const HighlightOverlay({
-    super.key,
-    required this.child,
-    required this.text,
-    required this.highlights,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    // If no highlights, return child as-is
-    if (highlights.isEmpty) {
-      return child;
-    }
-
-    // For now, return child directly
-    // TODO: Implement overlay positioning (complex - needs TextPainter calculations)
-    return child;
-  }
-}
-
 /// Information about a highlight region
 class HighlightInfo {
   final int offset;
