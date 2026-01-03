a4e7b3dd321282d9fca0b65ab304b7f12c6c64ea
Ivgeni Segal <ivgeni.segal@gmail.com>
2026-01-03 12:44:37 -0800

    Initial working secrets

--- agent-core/lib/src/services/shell_service.dart
+++ agent-core/lib/src/services/shell_service.dart
@@ -117,7 +117,8 @@ class ShellService {
     }
 
     try {
-      String finalCommand;
+      String commandToExecute;
+      String? scriptToPipe;
       final secretsToRedact = <String>[];
 
       final resolvedSecrets = await _resolveSecrets(secrets);
@@ -135,22 +136,32 @@ class ShellService {
 
             final encodedValue = base64.encode(utf8.encode(entry.value));
             envExports.writeln(
-              "export ${entry.key}=\$(echo '$encodedValue' | base64 -d)",
+              "export ${entry.key}=\"\$(echo '$encodedValue' | base64 -d)\"",
             );
             secretsToRedact.add(entry.value);
           }
         }
 
-        finalCommand = '''
-bash -c "source <(cat <<'DECAMP_SECRETS'
-${envExports}DECAMP_SECRETS
-) && ${_escapeForCommand(command)}"
+        scriptToPipe = '''
+$envExports
+$command
 ''';
+        commandToExecute = 'bash';
       } else {
-        finalCommand = command;
+        commandToExecute = command;
       }
 
-      final session = await _backend.execute(finalCommand);
+      _log.info('Executing command: $commandToExecute');
+      if (scriptToPipe != null) {
+        _log.info('Piping script: $scriptToPipe');
+      }
+
+      final session = await _backend.execute(commandToExecute);
+
+      if (scriptToPipe != null) {
+        session.write(utf8.encode(scriptToPipe));
+        session.close();
+      }
 
       StreamSubscription<ProcessSignal>? abortSubscription;
       if (abortSignal != null) {
@@ -193,7 +204,7 @@ ${envExports}DECAMP_SECRETS
       final exitCode = await session.exitCode;
 
       _log.info(
-        'Command executed. Exit code: $exitCode, stdout length: ${stdout.length}, stderr length: ${stderr.length}',
+        'Command executed. Exit code: $exitCode, stdout: $stdout, stderr: $stderr',
       );
 
       return {
@@ -300,48 +311,46 @@ ${envExports}DECAMP_SECRETS
           }
           final encodedValue = base64.encode(utf8.encode(entry.value));
           envExports.writeln(
-            "export ${entry.key}=\$(echo '$encodedValue' | base64 -d)",
+            "export ${entry.key}=\"\$(echo '$encodedValue' | base64 -d)\"",
           );
           secretsToRedact.add(entry.value);
         }
       }
     }
 
-    String secureCommand;
+    String secureScript;
 
     if (sudoPassword != null) {
       final askpassPath = '/tmp/decamp_askpass_\$\$';
       final encodedSudoPassword = base64.encode(utf8.encode(sudoPassword));
 
-      secureCommand = '''
-bash -c "
+      secureScript = '''
 (umask 077 && cat > $askpassPath <<'ASKPASS_EOF'
 #!/bin/sh
-echo \\\"\\\$SUDO_PASSWORD\\\"
+echo "\$SUDO_PASSWORD"
 ASKPASS_EOF
 )
 chmod 700 $askpassPath
 
-source <(cat <<'DECAMP_SECRETS'
-${envExports}export SUDO_PASSWORD=\$(echo '$encodedSudoPassword' | base64 -d)
-DECAMP_SECRETS
-) && SUDO_ASKPASS=$askpassPath sudo -A bash -c '${_escapeForCommand(command)}'
+$envExports
+export SUDO_PASSWORD="\$(echo '$encodedSudoPassword' | base64 -d)"
+SUDO_ASKPASS=$askpassPath sudo -A bash -c '${_escapeForCommand(command)}'
 
 rm -f $askpassPath
-"
 ''';
     } else {
-      secureCommand = '''
-bash -c "source <(cat <<'DECAMP_SECRETS'
-${envExports}DECAMP_SECRETS
-) && sudo bash -c '${_escapeForCommand(command)}'"
+      secureScript = '''
+$envExports
+sudo bash -c '${_escapeForCommand(command)}'
 ''';
     }
 
     _log.info('Executing sudo command: sudo $command (secrets redacted)');
 
     try {
-      final session = await _backend.execute(secureCommand);
+      final session = await _backend.execute('bash');
+      session.write(utf8.encode(secureScript));
+      session.close();
 
       StreamSubscription<ProcessSignal>? abortSubscription;
       if (abortSignal != null) {
@@ -657,6 +666,10 @@ class LocalShellBackend implements ShellBackend {
 
   @override
   Future<ShellSession> execute(String command) async {
+    if (command == 'bash') {
+      final process = await Process.start('bash', []);
+      return LocalShellSession(process);
+    }
     final process = await Process.start('bash', ['-c', command]);
     return LocalShellSession(process);
   }
@@ -693,7 +706,7 @@ class LocalShellSession implements ShellSession {
   void kill(ProcessSignal signal) => _process.kill(signal);
 
   @override
-  void close() => _process.kill();
+  void close() => _process.stdin.close();
 }
 
 class UnconfiguredShellBackend implements ShellBackend {
