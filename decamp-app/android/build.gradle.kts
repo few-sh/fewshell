6e84a20819689817217979d891f4ff71291978a5
Ivgeni Segal <ivgeni.segal@gmail.com>
2026-03-11 18:47:58 -0700

    Fix android build

--- decamp-app/android/build.gradle.kts
+++ decamp-app/android/build.gradle.kts
@@ -16,6 +16,30 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    if (project.name != "app") {
+        afterEvaluate {
+            if (project.plugins.hasPlugin("com.android.library")) {
+                project.extensions.configure<com.android.build.gradle.LibraryExtension> {
+                    if (namespace.isNullOrEmpty()) {
+                        val manifest = file("${project.projectDir}/src/main/AndroidManifest.xml")
+                        if (manifest.exists()) {
+                            val packageName = javax.xml.parsers.DocumentBuilderFactory.newInstance()
+                                .newDocumentBuilder()
+                                .parse(manifest)
+                                .documentElement
+                                .getAttribute("package")
+                            if (packageName.isNotEmpty()) {
+                                namespace = packageName
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
